8d177f196e1929963b8de9f2cc13efd18ac6c84a
hannasherin <hannasherinpp2000@gmail.com>
2026-08-16 08:08:47 +0530

    React

--- Week 5/Redux Thunk/src/pages/TaskList.jsx
+++ Week 5/Redux Thunk/src/pages/TaskList.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getTaskFromServer } from '../slice/taskSlice';
 
 const TaskList = () => {
+   const{tasks,isloading,error}= useSelector((state)=>state.taskInfo)
+   console.log(tasks);
+
+   const dispatch=useDispatch()
+
+   useEffect(()=>{
+    
+  dispatch(getTaskFromServer())
+   },[])
+   
   return (
     <div>
       <h1>Task List</h1>
--- Week 5/Redux Thunk/src/slice/taskSlice.js
+++ Week 5/Redux Thunk/src/slice/taskSlice.js
@@ -7,24 +7,45 @@ const initialState={
 }
 
 export const getTaskFromServer=createAsyncThunk(
-    'tasks/getTaskFromServer',
-    async(_,{rejectWithValue})=>{
+    'tasks/getTaskFromServer', //type
+    async(_,{rejectWithValue})=>{  //function rejectwithvalue- thunk api 
         try {
             const response=await api.get('/tasks')
             return response.data
 
         } catch (error) {
-            
+            return rejectWithValue('no task found')
         }
         
     }
 )
 
-const task=createSlice({
+const taskSlice=createSlice({
     name:'tasks',
     initialState,
     reducers:{},
     extraReducers:(builder)=>{
+       builder
+        //   pending 
+        .addCase(getTaskFromServer.pending , (state,action)=>{
+          state.isloading=true;
+          state.errorr='';
           
+        })
+        // success 
+        .addCase(getTaskFromServer.fulfilled,(state,action)=>{
+            state.isloading=false;
+            state.error='';
+            state.task=action.payload
+        
+        })
+        // error 
+        .addCase(getTaskFromServer.rejected,(state,action)=>{
+            state.isloading=false;
+            state.error=action.payload
+            state.tasks=[]
+        })
+
     }
 })
+export default  taskSlice.reducer
